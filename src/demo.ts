55338b33aba8947a9fd763d0e9601922025be05f
gherrick0918 <gherrick0918@gmail.com>
2025-10-17 14:15:07 -0400

    .gitignore and speeds

--- src/demo.ts
+++ src/demo.ts
@@ -5,7 +5,7 @@ import { DeviceProfile, makeIntervals, makeSteady, makeProgression, describe } f
 const profile: DeviceProfile = {
   name: "Greg's Walking Pad",
   units: "mph",
-  speeds: [1.0, 1.5, 2.0, 2.5, 3.0],
+  speeds: [0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8],
   minSegmentSec: 30
 };
 
